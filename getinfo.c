29adf877ca18c764d72363e5183d3beec39e9844
Yareda21 <ykebede39@yahoo.com>
2023-09-17 04:52:06 -0700

    a

--- getinfo.c
+++ getinfo.c
@@ -86,16 +86,3 @@ void free_info(info_t *info, int all)
 	}
 }
 
-
-/**
- * clear_info - initializes info_t struct
- * @info: struct address
- */
-void clear_info(info_t *info)
-{
-	info->arg = NULL;
-	info->argv = NULL;
-	info->path = NULL;
-	info->argc = 0;
-}
-
